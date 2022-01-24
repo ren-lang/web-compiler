2abf57400a12029812c6346fc25784d1513c287b
Andy Thompson <andrew.thompson@qmul.ac.uk>
2022-01-24 00:50:58 +0000

    :zap: Use a better compiler cache.

--- src/Main.elm
+++ src/Main.elm
@@ -1,30 +1,63 @@
 port module Main exposing (main)
 
+-- IMPORTS ---------------------------------------------------------------------
+
+import Dict exposing (Dict)
 import Ren.Compiler exposing (..)
-import Set exposing (Set)
 
 
-main : Program () (Set String) String
-main =
-    Platform.worker
-        { init = always ( Set.empty, Cmd.none )
-        , update =
-            \input cache ->
-                ( cache
-                , if Set.member input cache then
-                    Cmd.none
 
-                  else
-                    run untyped input
-                        |> Result.toMaybe
-                        |> Maybe.map toJavascript
-                        |> Maybe.withDefault Cmd.none
-                )
-        , subscriptions = always <| fromJavascript Basics.identity
-        }
+-- PORTS -----------------------------------------------------------------------
 
 
 port toJavascript : String -> Cmd msg
 
 
 port fromJavascript : (String -> msg) -> Sub msg
+
+
+
+-- MAIN ------------------------------------------------------------------------
+
+
+{-| It might seem a bit weird to keep `Maybe`s in a dictionary, but by doing this
+we save the effort of re-parsing invalid inputs. If they're `Nothing` we know
+something went wrong!
+
+In the future we should probably store the compiler error instead, using a `Result`
+so that we can report it to users, but this gets the job done for now.
+
+-}
+type alias Cache =
+    Dict String (Maybe String)
+
+
+main : Program () Cache String
+main =
+    Platform.worker
+        { init =
+            \_ -> ( Dict.empty, Cmd.none )
+        , update =
+            \input cache ->
+                if Dict.member input cache then
+                    ( cache
+                    , Dict.get input cache
+                        |> Maybe.andThen Basics.identity
+                        |> Maybe.map toJavascript
+                        |> Maybe.withDefault Cmd.none
+                    )
+
+                else
+                    case run untyped input |> Result.toMaybe of
+                        Just esm ->
+                            ( Dict.insert input (Just esm) cache
+                            , toJavascript esm
+                            )
+
+                        Nothing ->
+                            ( Dict.insert input Nothing cache
+                            , Cmd.none
+                            )
+        , subscriptions =
+            \_ -> fromJavascript Basics.identity
+        }
